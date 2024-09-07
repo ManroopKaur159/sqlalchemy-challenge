779724dcb84aa9ec81d313635ed9567125811680
Manroop Kaur <manroopkaur646@gmail.com>
2024-09-07 14:48:11 -0400

    final changes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -37,13 +37,14 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def welcome():
-    """List all available api routes."""
+    """List all the available api routes on this app."""
     return (
         f"Welcome to the SQL-Alchemy APP API!<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/tobs/stats<br/>"
         f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
         f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
     )
